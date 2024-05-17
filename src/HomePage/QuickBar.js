0afbe597a7f1398a5440b3462afcbc723ef1e7c1
SKX Kafei <josue.ham@unitec.edu>
2024-05-17 03:09:40 -0600

    Error de referencia de VideoCard y VideoThumbnail

--- src/HomePage/QuickBar.js
+++ src/HomePage/QuickBar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import VideoThumbnail from './Atoms/VideoThumbnail';
+import VideoThumbnail from "../Atoms/VideoThumbnails";
 
 const RecommendationsContainer = styled.div`
   display: flex;
--- src/HomePage/VideoSections.js
+++ src/HomePage/VideoSections.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import VideoCard from './HomePage/VideoCard';
+import VideoCard from '../Molecules/VideoCard';
 
 const SectionContainer = styled.div`
   margin-top: 20px;
